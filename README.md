b7ef9ae2e85892a65cf2cb0c94b5ebe819dda3cb
Khang <khangn7@educbe.ca>
2023-03-18 00:36:47 -0600

    updated all files
    first send message test success

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # api assignment
+
+blank user json blob 
+
+{
+    "taken": false
+}
+
+init user json blob
+
+{
+    "taken": true,
+    "messages": [ ],
+    "username": < username >
+}
+
+message object pushed to user_json_blob[ "messages" ]
+
+{
+    "content": ,
+    "time"
+}
--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const BLOB_URL = "https://jsonblob.com/api/jsonBlob/1085945016400232448";
+const API_URL = "https://jsonblob.com/api/jsonBlob/";
 
-async function getJSONBlob(){
+const USERS = [
+    {
+        "endpoint": "1085945016400232448"
+    },
+    {
+        "endpoint": "1086508883706658816"
+    }
+];
 
-    let response = await fetch(BLOB_URL, {
+
+async function getJSONBlob(blob_endpoint){
+
+    let response = await fetch(API_URL + blob_endpoint, {
         method: "GET",
         headers: {
             accept: "application/json",
@@ -16,9 +26,9 @@ async function getJSONBlob(){
 }
 
 
-async function updateJSONBlob(content_obj){
+async function updateJSONBlob(blob_endpoint, content_obj){
 
-    let response = await fetch(BLOB_URL, {
+    let response = await fetch(API_URL + blob_endpoint, {
         method: "PUT",
         headers: {
             accept: "application/json",
@@ -32,70 +42,210 @@ async function updateJSONBlob(content_obj){
     return data; 
 }
 
-function endSession(){
 
-    updateJSONBlob({
-        "messages": [],
-        "users": [],
-        "new-message": false
-    });
-}
+
 
 
 const username_box = document.getElementById("username-box");
 
 const username_button = document.getElementById("username-button");
-username_button.addEventListener("click", init_user);
+username_button.addEventListener("click", initUser);
 
-const form = document.getElementById("send-form");
+const message_form = document.getElementById("send-form");
 
 const messages_box = document.getElementById("messages-box");
 
-const error_message = document.getElementById("error-message");
+const error_box = document.getElementById("error-message");
 
 
-async function init_user(){
+
+
+let my_user_name, 
+    my_user_endpoint,
+    other_user_endpoint,
+    other_message_count;
+
+async function initUser(){
 
     if(username_box.value.trim() === ""){
 
-        error_message.innerHTML = "invalid username";
+        error_box.innerHTML = "invalid username";
         return;
     }
     
     username_box.style.display = "none";
     username_button.style.display = "none";
 
+    error_box.style.display = "block";
+    error_box.innerHTML = "attempting to connect..."
 
-    let _json_blob = await getJSONBlob();
+    //get user blobs
 
-    //create user id, username,
-    //then push username to "users": []  of jsonBlob
+    let chat_full = true;
 
-    const user_id = _json_blob["users"].length;
+    for(let i = 0; i < USERS.length; i++){
 
-    const user_name = username_box.value.trim();
+        let USERS_endpoint = USERS[i]["endpoint"];
 
-    _json_blob["users"].push(user_name);
+        let USERS_blob = await getJSONBlob(USERS_endpoint);
 
-    let _init_response = await updateJSONBlob(_json_blob);
+        //if blob is not taken
+        if(!USERS_blob["taken"]){
 
-    //show message log div and form
-    form.style.display = "block";
-    messages_box.style.display = "block"; 
+            my_user_endpoint = USERS_endpoint;
 
-}
+            my_user_name = username_box.value.trim();
 
-function sendMessage(user_text){
+            let _flip = i === 0 ? 1 : 0;
 
-
-}
+            other_user_endpoint = USERS[_flip]["endpoint"];
 
 
-// start routine fetching json_blob to check for new messages on first message
-form.onsubmit = (user_text)=> {
-    user_text.preventDefault
-    
-    let user_obj = {
+            //update this blob
 
+            USERS_blob["taken"] = true;
+
+            USERS_blob["messages"] = [];
+
+            USERS_blob["username"] = my_user_name;
+            
+            await updateJSONBlob(USERS_endpoint, USERS_blob);
+
+            chat_full = false;
+
+            break;
+        }
     }
+
+    // if all json blobs are taken
+    if(chat_full){
+
+        error_box.innerHTML = "ppl are already using";
+        return;
+    }
+
+    error_box.style.display = "none";
+    
+    messages_box.style.display = "block";
+    message_form.style.display = "block";
+
+    end_session_button.style.display = "block";
+
+    // start routinely fetching json_blob to check for new messages, 
+    // or if other user connected
+
+    fetch_interval = setInterval(checkOtherUserBlob, 10000);
+
+    other_message_count = 0;
+
+}
+
+const end_session_button = document.getElementById("end-session");
+end_session_button.addEventListener("click", endSession);
+
+async function endSession(){
+
+    await updateJSONBlob(my_user_endpoint, {
+        "taken": false
+    });
+
+    //reset stage
+}
+
+function displayMessage(content_str, time_str){
+
+    let paragraph = document.createElement("p");
+
+    paragraph.class = "display-message";
+
+    paragraph.innerHTML = time_str + '\n' + content_str;
+
+    messages_box.appendChild(paragraph);
+
+}
+
+function getTimeStr(){
+
+    let date = new Date();
+
+    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+
+const send_button = document.getElementById("send-button");
+
+const msg_input_box = document.getElementById("message-input-box");
+
+send_button.addEventListener("click", messageSubmit);
+
+let other_user_connected = false;
+
+let fetch_interval;
+
+function messageSubmit(){
+    
+    if(!other_user_connected){
+
+        console.log("no one listening")
+
+        return;
+    }
+
+    console.log("sending msg");
+
+    sendMessage(msg_input_box.value);
+
+}
+
+async function sendMessage(content_str){
+
+    let my_blob = await getJSONBlob(my_user_endpoint);
+
+    let _msg = {
+        "content": content_str,
+        "time": getTimeStr()
+    }
+
+    my_blob["messages"].push(_msg)
+
+    await updateJSONBlob(my_user_endpoint, my_blob);
+
+    return true;
+
+}
+
+async function checkOtherUserBlob(){
+
+    let other_json = await getJSONBlob(other_user_endpoint);
+
+    //if other user has connected
+    if(other_json["taken"]){
+
+        other_user_connected = true;
+
+    } else {
+
+        return;
+    }
+
+
+    // if other user messages have been updated
+    let _other_messages_length = other_json["messages"].length;
+
+    if(_other_messages_length !== other_message_count){
+
+        let _other_messages = other_json["messages"];
+
+        // add new messages
+
+        for(let i = other_message_count; i < _other_messages_length; i++){
+
+            let _message = _other_messages[i];
+
+            displayMessage(_message["content"], _message["time"]);
+        }
+
+        other_message_count = _other_messages_length;
+    }
+
 }
